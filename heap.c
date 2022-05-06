b5884b04a355f1e2be80670c2cf0dc1974d39959
Your Name <you@example.com>
2022-05-06 17:05:56 -0400

    new try

--- heap.c
+++ heap.c
@@ -70,10 +70,10 @@ void heap_pop(Heap* pq)
 
 }
 
-Heap* createHeap(){
+Heap* createHeap()
+{
   Heap *monticulo = (Heap*) malloc(sizeof(Heap));
-  monticulo -> heapArray = malloc(3*sizeof(heapElem));
+  monticulo -> heapArray = malloc(sizeof(heapElem));
   monticulo -> capac = 3;
-  
-   return monticulo;
+  return monticulo;
 }
