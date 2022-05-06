e00ed0673cb52eed9f0355843c516fd4e079e902
JanHH16 <>
2022-05-06 15:34:16 +0000

    new try

--- heap.c
+++ heap.c
@@ -17,7 +17,8 @@ typedef struct Heap{
 } Heap;
 
 
-void* heap_top(Heap* pq){
+void* heap_top(Heap* pq)
+{
     if(pq->size != 0)
       return pq->heapArray[0].data;
     else return NULL;
@@ -25,13 +26,14 @@ void* heap_top(Heap* pq){
 
 
 
-void heap_push(Heap* pq, void* data, int priority){
-  
+void heap_push(Heap* pq, void* data, int priority)
+{
 
 }
 
 
-void heap_pop(Heap* pq){
+void heap_pop(Heap* pq)
+{
 
 }
 
